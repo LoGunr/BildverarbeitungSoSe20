d9251a858746bbb4055ccf2b48db8fc9bada0cb3
Looo911 <lo.gunreben@gmail.com>
2020-04-21 20:25:01 +0300

    Aufgabe 2_2

--- aufgabe2_2.py
+++ aufgabe2_2.py
@@ -1,4 +1,4 @@
-6# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Spyder Editor
 
@@ -7,4 +7,30 @@ Dies ist eine temporäre Skriptdatei.
 
 import matplotlib.pyplot as plt
 import numpy as np 
-import opencv
+import cv2
+
+img = cv2.imread('test.png')
+plt.figure(dpi=200)
+kernel = np.ones((5,5),np.float32)/25
+
+central_value = 130.0
+
+kernel2 = np.ones((5,5),np.float32)
+
+kernel2[2][2] = central_value
+
+kernel2 /= 24.0 + central_value
+
+print(kernel2)
+#The higher the central_value, the less is the
+
+dst = cv2.filter2D(img,-1,kernel)
+dst2 = cv2.filter2D(img,-1,kernel2)
+
+plt.subplot(231),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(232),plt.imshow(dst),plt.title('Averaging')
+plt.xticks([]), plt.yticks([])
+plt.subplot(233),plt.imshow(dst2),plt.title('High Center')
+plt.xticks([]), plt.yticks([])
+plt.show()
